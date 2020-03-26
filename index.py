e0cdf8cc15da97acc7071da63941b4f0918cf7ee
GustavoHdezH <gushdezh@hotmail.com>
2020-03-26 11:32:46 -0600

    Modificacion de archivo

--- index.py
+++ index.py
@@ -42,3 +42,46 @@ except:
     conectar.rollback()
 finally:
     conectar.close()
+
+"""
+    Read record
+"""
+#query = "SELECT * FROM persona;"
+#
+#operation = conn.cursor()
+#operation.execute(query)
+#
+#while True:
+#    record = operation.fetchone()
+#    if record == None:
+#        break
+#    print(record)
+#conn.close()
+
+"""
+    Update record
+"""
+#query = "update persona set nombre=?, edad=? where id_persona=4;"
+#try:
+#    operation = conn.cursor()
+#    operation.execute(query,('Manuel',24))
+#    conn.commit()
+#    print("Registo actualizado correctamente")
+#except:
+#    print("Error al actualizar registro")
+#    conn.rollback()
+#conn.close()
+
+"""
+    Delete record
+"""
+#query = "DELETE FROM persona where id_persona=?;"
+#try:
+#    operation = conn.cursor()
+#    operation.execute(query,(4,))
+#    conn.commit()
+#    print ("registro elmiminado correctamente")
+#except:
+#    print("Error al borrar registro")
+#    conn.rollback()
+#conn.close()
